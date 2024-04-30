efab293eec2645129855a0b0b539537dacb6bebd
OnkarKale1405 <onkarkale0007@gmail.com>
2024-04-30 15:58:09 +0530

    intial commit

---
+++ src/constants.js
@@ -0,0 +1,6 @@
+export const roles = {
+    student: 2000,
+    teacher: 2001
+}
+
+export const DB_NAME = "Something"
---
+++ src/routes/user.routes.js
@@ -0,0 +1,41 @@
+import { Router } from "express";
+import {
+    loginUser,
+    logoutUser,
+    registerUser,
+    refreshAccessToken,
+    getCurrentUser,
+    updateAccountDetails,
+    updateUserAvatar,
+} from "../controllers/user.controller.js";
+
+// middlewares
+import { upload } from "../middlewares/multer.middleware.js"
+import { verifyJWT } from "../middlewares/auth.middleware.js";
+
+const router = Router();
+
+router.route("/register").post(
+    upload.fields([
+        {
+            name: "avatar",
+            maxCount: 1
+        }
+    ]),
+    registerUser
+);
+router.route("/login").post(loginUser);
+
+// secured routes
+router.route("/logout").post(verifyJWT, logoutUser);
+router.route("/refresh-token").post(verifyJWT, refreshAccessToken);
+router.route("/user").get(verifyJWT, getCurrentUser);
+router.route("/update-details").patch(verifyJWT, updateAccountDetails);
+router.route("/update-avatar")
+    .patch(
+        verifyJWT,
+        upload.single("avatar"),
+        updateUserAvatar
+    );
+
+export default router
